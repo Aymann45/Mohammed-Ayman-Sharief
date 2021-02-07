5ef0a704d68ff6841c7131f8db5747b00ea6a680
Aymann45 <76143116+Aymann45@users.noreply.github.com>
2021-02-07 17:54:54 +0530

    Update sum_of_numbers.c

--- sum_of_numbers.c
+++ sum_of_numbers.c
@@ -1,11 +1,1 @@
 //Write a program to find the sum of n different numbers using 4 functions
-
-#include<stdio.h>
-int main() 
-{
-   float a, b, sum;
-   printf("Enter two numbers a and b = ");
-   scanf("%f %f", &a, &b);
-   sum = a + b;
-   printf("Sum of %f + %f = %f", a,b,sum);
-   return(0)
