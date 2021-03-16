904a2bbd278af52e5d9f7342664cf6615c52bb4b
Aymann45 <76143116+Aymann45@users.noreply.github.com>
2021-03-16 10:36:41 +0530

    Update distance.c

--- distance.c
+++ distance.c
@@ -3,34 +3,28 @@
 #include <stdlib.h>
 #include <math.h>
 
-float input(char c)
+float distance_calculator(float x1,float x2, float y1, float y2)
 {
-  float n;
-  printf("Enter the point %c:",h);
-  scanf("%f",&n);
-  return n;
+  float distance;
+  distance=sqrt((x1-y1)*(x1-y1)+(x2-y2)*(x2-y2));
 }
-
-float distance (float x1,float y1,float x2,float y2)
-{
-  float dis;
-  dis=sqrt((x2-x1)(x2-x1)+(y2-y1)(y2-y1));
-  return dis;
-}
-
-void output (float dis)
-{
-  printf("Distance between (x1,y1) and (x2,y2) is: %0.2f",dis);
-}
-
 int main()
 {
-  float dis,a,b,c,d;
-  a=input('x');
-  b=input('y');
-  c=input('x');
-  d=input('y');
-  dis=distance(a,b,c,d);
-  output(dis);
+  float result,a,b,c,d;
+  printf("\nEnter the coordinates of point A:\n"):
+  printf("\nX-Axis Coordinate:\t");
+  scanf("%f,&b");
+  printf("\nENter the coordinates of point B:\n");
+  printf("\nY-Axis coordinate:\t");
+  scanf("%f",&c);
+  printf("\nY-Axis coordinate:\t");
+  scanf("%f",&d);
+  result=distance_calculator(a,b,c,d);
+  printf("\ndistance between points A nad B=%f\n",result);
+  return 0;
 }
   
+        
+    
+         
+
