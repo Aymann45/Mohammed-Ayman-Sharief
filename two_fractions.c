7c407b6fe204c734f252b70e79f1d14096e22d61
Aymann45 <76143116+Aymann45@users.noreply.github.com>
2021-03-17 18:13:33 +0530

    Update two_fractions.c

--- two_fractions.c
+++ two_fractions.c
@@ -1,1 +1,25 @@
 //WAP to find the sum of two fractions.
+#include<stdio.h>
+#include<conio.h>
+typeof struct
+{
+  int num;
+  int deno;
+}
+fract;
+fract sum(fract.fract);
+int main()
+{
+  int num1,deno1,num2,deno2;
+  printf("Enter 1st fraction=numerator and denominator");
+  scanf("%d%d",&num1,&deno1);
+  printf("Enter 2nd fraction= numerator and denominator");
+  scanf("%d,%d",&num2,deno2);
+  fract f1={num1,deno1};
+  fract f2={num2,deno2};
+  fract result=sum(f1,f2);
+  printf("Result=%d%d",result,num,result,deno);
+  getch();
+  return 0;
+}
+         
